44c7f7fd7e94448adfcf78ea0bdf02b6e29d50a2
purple-eye-orange <12sh20@naver.com>
2024-07-23 23:48:58 +0900

    css 색상 설정, 폴더 구조 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,62 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        lola: {
+          50: 'hsl(var(--lola-50))',
+          100: 'hsl(var(--lola-100))',
+          200: 'hsl(var(--lola-200))',
+          300: 'hsl(var(--lola-300))',
+          400: 'hsl(var(--lola-400))',
+          500: 'hsl(var(--lola-500))',
+          600: 'hsl(var(--lola-600))',
+          700: 'hsl(var(--lola-700))',
+          800: 'hsl(var(--lola-800))',
+          900: 'hsl(var(--lola-900))',
+          950: 'hsl(var(--lola-950))',
+        },
+        conblue: {
+          50: 'hsl(var(--conblue-50))',
+          100: 'hsl(var(--conblue-100))',
+          200: 'hsl(var(--conblue-200))',
+          300: 'hsl(var(--conblue-300))',
+          400: 'hsl(var(--conblue-400))',
+          500: 'hsl(var(--conblue-500))',
+          600: 'hsl(var(--conblue-600))',
+          700: 'hsl(var(--conblue-700))',
+          800: 'hsl(var(--conblue-800))',
+          900: 'hsl(var(--conblue-900))',
+          950: 'hsl(var(--conblue-950))',
+        },
+        memo: {
+          yellow: {
+            DEFAULT: 'hsl(var(--memo-yellow))',
+            text: 'hsl(var(--memo-yellow-text))',
+            button: 'hsl(var(--memo-yellow-button))',
+          },
+          green: {
+            DEFAULT: 'hsl(var(--memo-green))',
+            text: 'hsl(var(--memo-green-text))',
+            button: 'hsl(var(--memo-green-button))',
+          },
+          purple: {
+            DEFAULT: 'hsl(var(--memo-purple))',
+            text: 'hsl(var(--memo-purple-text))',
+            button: 'hsl(var(--memo-purple-button))',
+          },
+          pink: {
+            DEFAULT: 'hsl(var(--memo-pink))',
+            text: 'hsl(var(--memo-pink-text))',
+            button: 'hsl(var(--memo-pink-button))',
+          },
+          button: {
+            gray: {
+              DEFAULT: 'hsl(var(--memo-button-gray))',
+              hover: 'hsl(var(--memo-button-gray-hover))',
+            },
+          },
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
