1a35b5c0dce7085d6dd7de53b7d24eb4bc0919cc
purple-eye-orange <12sh20@naver.com>
2024-07-27 17:53:09 +0900

    add login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Layer from '@/components/Layer'
 import LandingMemo from '@/components/Landing_Memo'
+import LogInButton from '@/components/LogInButton'
 
 const Login = () => {
   return (
     <div>
       <Layer>
-        <div>Login</div>
-        <LandingMemo>dkdkdk</LandingMemo>
-        <LandingMemo color={'blue'}>dkdkdk</LandingMemo>
+        <div className="relative w-96 top-40 mx-auto">
+          <LandingMemo>dkdkdk</LandingMemo>
+          <LandingMemo color={'blue'} classname="absolute left-16 top-16">
+            dkdkdk
+          </LandingMemo>
+          <LogInButton />
+        </div>
       </Layer>
     </div>
   )
