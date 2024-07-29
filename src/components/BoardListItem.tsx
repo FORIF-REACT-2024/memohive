b3313753b2bbd9491f3f56abb8e571dd855ad04e
purple-eye-orange <12sh20@naver.com>
2024-07-29 16:00:00 +0900

    add boardlistitem component

--- src/components/BoardListItem.tsx
+++ src/components/BoardListItem.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import DeleteButton from './DeleteButton'
 
 const BoardListItem = () => {
   return (
-    <div className="w-boardlistitem h-boardlistitem p-6 rounded-xl bg-gradient-to-br from-lola-100 to-conblue-100/70 drop-shadow-md">
-      <input
-        type="text"
-        placeholder="Board1"
-        className="w-full bg-transparent placeholder:text-2xl placeholder:text-conblue-800"
-      />
+    <div className="w-boardlistitem h-boardlistitem p-6 rounded-xl bg-gradient-to-br from-lola-100 to-conblue-100/70 drop-shadow-md flex flex-col gap-2">
+      <div className="flex items-center justify-between">
+        <form action="submit">
+          <input
+            type="text"
+            placeholder="Title"
+            className="w-32 bg-transparent text-2xl text-conblue-800 placeholder:text-conblue-800 outline-none"
+          />
+        </form>
+        <DeleteButton />
+      </div>
+      <form action="submit">
+        <input
+          type="text"
+          placeholder="보드 설명"
+          className="w-full h-full bg-transparent text-xl text-conblue-700/60 placeholder:text-conblue-700/60 outline-none"
+        />
+      </form>
       <p></p>
     </div>
   )
