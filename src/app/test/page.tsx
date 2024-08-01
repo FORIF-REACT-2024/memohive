3b203a3e87728e4802c1abe3b96ef402bcaa97a5
Seongjin Cho <42935823+seongjinemong@users.noreply.github.com>
2024-08-01 22:44:46 +0900

    Example for using prisma

---
+++ src/app/test/page.tsx
@@ -0,0 +1,15 @@
+import { createUser, getUser } from '@api/user'
+import ChangeUserName from './component'
+
+export default async function Test() {
+  const user: string = await getUser('66aa522093d7601408509951').then((res) => {
+    return res.data.username
+  })
+
+  return (
+    <div className="w-full flex justify-center items-center flex-col mt-20">
+      <div className="text-center">Test</div>
+      <ChangeUserName name={user} />
+    </div>
+  )
+}
