aa6413aed0bc97a871f7544f583871749d15da91
Seongjin Cho <42935823+seongjinemong@users.noreply.github.com>
2024-08-03 20:57:19 +0900

    Board page changed to get memos from database

---
+++ src/app/board/page.tsx
@@ -0,0 +1,8 @@
+export default function Board() {
+  return (
+    <div className="w-full flex items-center flex-col h-svh mt-36 text-3xl gap-6">
+      <div>This page is /board</div>
+      <div>Not a right path</div>
+    </div>
+  )
+}
