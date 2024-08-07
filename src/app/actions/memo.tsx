b89500a8b4165301fc83db2690d8df2613a1470c
Seongjin Cho <42935823+seongjinemong@users.noreply.github.com>
2024-08-07 10:52:52 +0900

    Board page changed to Server Compoenent

--- src/app/actions/memo.tsx
+++ src/app/actions/memo.tsx
@@ -7,6 +7,8 @@
 import prisma from '@/lib/db'
 import CustomResponse from '@/lib/res'
 import makeObject from './makeobject'
+import { Prisma } from '@prisma/client'
+import { notFound } from 'next/navigation'
 
 /**
  * TODO 본인 소유의 보드인지 확인, 아니라면 isPublic 확인 후 true 라면 추가
@@ -45,6 +47,11 @@ export async function getBoardMemos(boardId: string) {
 
     return makeObject(new CustomResponse('200', 'Success', memos))
   } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === 'P2023') {
+        return makeObject(new CustomResponse(e.code, 'No Board Found'))
+      }
+    }
     return makeObject(new CustomResponse(e.code, e.message))
   }
 }
